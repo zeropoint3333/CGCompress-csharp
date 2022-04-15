f49c4a9d9f6bd34fed2013063ba4ba14dbbcc1b0
zeropoint3333 <zeropoint3333@hotmail.com>
2022-04-15 15:39:40 +0800

    调用OpenCv进行图像差分

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,16 +13,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using OpenCvSharp;
+
 namespace CGCompress
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Mat source1 = new Mat(@"resource\01.png", ImreadModes.Color);
+            Mat source2 = new Mat(@"resource\02.png", ImreadModes.Color);
+            Mat diff = new Mat();
+            Cv2.Absdiff(source1, source2, diff);
+            Cv2.ImShow("Demo", diff);
+            Cv2.WaitKey(0);
         }
     }
 }
