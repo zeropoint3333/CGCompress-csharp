4baf3f7cebb1ba9970dcf45406d8ee99d2e3b7d5
zeropoint3333 <zeropoint3333@hotmail.com>
2022-07-14 21:19:48 +0800

    Improve fileexplorer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,23 +21,16 @@ namespace CGCompress
 {
     public partial class MainWindow : System.Windows.Window
     {
-        private static class common
-        {
-            static String path = (@"E:\test");
-
-            public static string Path
-            {
-                get { return path; }
-                set { path = value; }
-            }
-        }
+        private ViewModels.MainWindowViewModel mainWindowViewModel = new ViewModels.MainWindowViewModel();
         public MainWindow()
         {
             InitializeComponent();
-            DirectoryInfo folder = new DirectoryInfo(common.Path);
-            this.Path_TextBox.Text = common.Path;
-            FileSystemInfo[] fileinfo = folder.GetFileSystemInfos();
-            FileExplorer.ItemsSource = fileinfo;
+            this.DataContext = mainWindowViewModel;
+            mainWindowViewModel.Path = System.IO.Directory.GetCurrentDirectory();
+
+            Style rowStyle = new Style(typeof(DataGridRow));
+            rowStyle.Setters.Add(new EventSetter(DataGridRow.MouseDoubleClickEvent, new MouseButtonEventHandler(FileExplorerRow_DoubleClick)));
+            FileExplorer.RowStyle = rowStyle;
 
         }
 
@@ -61,17 +54,13 @@ namespace CGCompress
             openFolderDialog.IsFolderPicker = true;
             if (openFolderDialog.ShowDialog() == Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
             {
-                common.Path = openFolderDialog.FileName;
-                DirectoryInfo folder = new DirectoryInfo(common.Path);
-                this.Path_TextBox.Text = common.Path;
-                FileSystemInfo[] subdir = folder.GetFileSystemInfos();
-                FileExplorer.ItemsSource = subdir;
+                mainWindowViewModel.Path = openFolderDialog.FileName;
             }
         }
 
         private void Compress_Click(object sender, RoutedEventArgs e)
         {
-            DirectoryInfo folder = new DirectoryInfo(common.Path);
+            DirectoryInfo folder = new DirectoryInfo(mainWindowViewModel.Path);
             ArrayList imgpaths = new ArrayList();
 
             //读取所有图片格式的文件，包括jpg png jpeg等
@@ -81,7 +70,7 @@ namespace CGCompress
                 imgpaths.Add(file.FullName);
             }
 
-            Views.CompressConfigDialog compressorConfig = new Views.CompressConfigDialog(this.Path_TextBox.Text);
+            Views.CompressConfigDialog compressorConfig = new Views.CompressConfigDialog(mainWindowViewModel.Path);
             if (compressorConfig.ShowDialog() == true)
             {
                 ImagePack.Compress(imgpaths, Convert.ToInt32(compressorConfig.subtracttimes.Text), compressorConfig.outpath.Text,"."+compressorConfig.Format.Content);
@@ -91,16 +80,16 @@ namespace CGCompress
 
         private void Extract_Click(object sender, RoutedEventArgs e)
         {
-            if(!File.Exists(this.Path_TextBox.Text + "\\compress_info.xml"))
+            if(!File.Exists(mainWindowViewModel.Path + "\\compress_info.xml"))
             {
                 MessageBox.Show("该目录下无压缩信息");
             }
             else
             {
-                Views.DecompressConfigDialog decompressorConfig = new Views.DecompressConfigDialog(this.Path_TextBox.Text);
+                Views.DecompressConfigDialog decompressorConfig = new Views.DecompressConfigDialog(mainWindowViewModel.Path);
                 if (decompressorConfig.ShowDialog() == true)
                 {
-                    ImagePack.Decompress(this.Path_TextBox.Text,decompressorConfig.outpath.Text,"."+decompressorConfig.Format.Content);
+                    ImagePack.Decompress(mainWindowViewModel.Path, decompressorConfig.outpath.Text,"."+decompressorConfig.Format.Content);
                 }
                 GC.Collect();
             }
@@ -108,31 +97,44 @@ namespace CGCompress
 
         private void SeeImage_Click(object sender, RoutedEventArgs e)
         {
+            //Determine if an item is selected
             if (FileExplorer.SelectedCells.Count == 0) return;
+
             String imgname = (FileExplorer.Columns[0].GetCellContent(FileExplorer.Items[this.FileExplorer.SelectedIndex]) as TextBlock).Text;
-            if (File.Exists(this.Path_TextBox.Text + "\\compress_info.xml"))
+            if (File.Exists(mainWindowViewModel.Path + "\\compress_info.xml"))
             {
                 System.Data.DataSet ds = new System.Data.DataSet();
-                ds.ReadXml(this.Path_TextBox.Text + "\\compress_info.xml");
+                ds.ReadXml(mainWindowViewModel.Path + "\\compress_info.xml");
                 System.Data.DataTable Pictures = ds.Tables[0];
                 int index = Convert.ToInt32(imgname.Substring(0,imgname.IndexOf(".")));
                 string imgtype = imgname.Substring(imgname.IndexOf("."),imgname.Length-imgname.IndexOf("."));
-                Mat img1 = Cv2.ImRead(this.Path_TextBox.Text + "\\" + index.ToString() + imgtype);
+                Mat img1 = Cv2.ImRead(mainWindowViewModel.Path + "\\" + index.ToString() + imgtype);
                 if (Convert.ToInt32((String)Pictures.Rows[index]["Father"]) < 0)
                 {
                     Cv2.ImShow("image" , img1);
                 }
                 else
                 {
-                    Mat img2 = Cv2.ImRead(this.Path_TextBox.Text + "\\" + (string)Pictures.Rows[index]["Father"] + imgtype);
+                    Mat img2 = Cv2.ImRead(mainWindowViewModel.Path + "\\" + (string)Pictures.Rows[index]["Father"] + imgtype);
                     Cv2.ImShow("image", ImageTool.Add_Mold(img2, img1));
                 }
                 img1.Release();
             }
             else
             {
-                Cv2.ImShow("image", Cv2.ImRead(this.Path_TextBox.Text+"\\"+imgname));
+                Cv2.ImShow("image", Cv2.ImRead(mainWindowViewModel.Path + "\\"+imgname));
             }
+        }
+
+        private void FileExplorerRow_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridRow? row = sender as DataGridRow;
+            if (Directory.Exists(((FileSystemInfo)row.Item).FullName)) mainWindowViewModel.Path = ((FileSystemInfo)row.Item).FullName;
+        }
+
+        private void MoveUp_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindowViewModel.Path = mainWindowViewModel.Path.Substring(0, mainWindowViewModel.Path.LastIndexOf("\\"));
         }
     }
 }
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGCompress.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string path= "";
+        public  string Path
+        {
+            get { return path; }
+            set { 
+                path = value;
+                OnPropertyChanged("Path");
+                OnPropertyChanged("FileInfos");
+            }
+        }
+
+        public FileSystemInfo[] FileInfos
+        {
+            get {
+                DirectoryInfo folder = new DirectoryInfo(Path);
+                FileSystemInfo[] subdir = folder.GetFileSystemInfos();
+                return subdir;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
